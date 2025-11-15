727a6ebfd5288dd4efa7beb2b23529d91437ccd3
Thiagonogsl <thiagonsilva06@gmail.com>
2025-11-15 18:30:21 -0300

    Cria as funções de geração aleatória

---
+++ etapa1_geracao.py
@@ -0,0 +1,86 @@
+import random
+import datetime
+
+#Dados escolhdos: Nome(str), Sobrenome(str), Idade(int), Telefone(str), Numero do Quarto(int), diaria_quarto(Float), 
+#Contatos de Emergencia(Lista), data_checkin, data_checkout, pagamento_Confirmado(Bool)
+
+#Gerar Datas de Check-in e check-out
+def gerar_dia_checkin():        
+    data_inicial = datetime.date(day=1, month=1, year=2025)
+    numero_dias = random.randint(0, 365)
+        
+    data_checkin = data_inicial + datetime.timedelta(days=numero_dias)
+    return data_checkin
+
+#Gerar Numeros de telefone
+def gerar_telefone():
+    ddd = random.randint(11, 99)
+
+    primeiros_numeros = random.randint(0, 9999)
+    ultimos_numeros = random.randint(0, 9999)
+    
+    primeiros_numeros = str(primeiros_numeros).zfill(4)
+    ultimos_numeros = str(ultimos_numeros).zfill(4)
+
+    telefone = (f"({ddd}) 9{primeiros_numeros}-{ultimos_numeros}")
+
+def gerar_numero_quarto():
+    andar = random.randint(3, 12)
+    andar = str(andar)
+
+    numero = random.randint(0, 20)
+    numero = str(numero).zfill(2)
+
+    numero_quarto = andar + numero
+    return int(numero_quarto)
+
+def gerar_nome():
+    lista_nomes = ["Thiago", "Lucas", "Marcos", "Antônio", "Gabriel", "João Pedro", "Juan", "Davi", "Claudio",\
+                "Igor", "Icaro", "Caio", "Eduardo", "Maria", "Victoria", "Ana", "Claudia", "Nicole", \
+                "Fernanda", "Michelle","Julia", "Mariana", "Aline", "Paula", "Patricia", "Giorgian"]
+    
+    lista_sobrenomes = ["Silva", "Santos", "Ferreira", "Oliveira", "Souza", "Rodrigues", "Alves", "Pereira",\
+                    "Lima", "Gomes", "Costa", "Ribeiro", "Arrascaeta", "Barbosa", "Martins", "Carvalho" ]
+
+    nome = random.choice(lista_nomes)
+    sobrenome = random.choice(lista_sobrenomes)
+
+    nome_completo = nome + " " + sobrenome
+    return nome_completo
+
+def gerar_precos():
+    lista_Precos = [199.99, 349.99, 650.00, 1300.00]
+    preco = random.choice(lista_Precos)
+
+    return preco
+
+def gerar_idade():
+    idade = random.randint(18, 80)
+    return idade
+
+def gerar_confirmacao_pagamento():
+    pagamento = bool(random.randint(0,1))
+    return pagamento
+
+print(gerar_confirmacao_pagamento())
+
+#Formatar as datas
+data_checkin = gerar_dia_checkin()
+dias_estadia = random.randint(1, 15)
+data_checkout = data_checkin + datetime.timedelta(days=dias_estadia)
+
+data_checkin = data_checkin.strftime("%d/%m/%Y")
+data_checkout = data_checkout.strftime("%d/%m/%Y")
+
+
+def gerar_telefone():
+    ddd = random.randint(11, 99)
+
+    primeiros_numeros = random.randint(0, 9999)
+    ultimos_numeros = random.randint(0, 9999)
+    
+    primeiros_numeros = str(primeiros_numeros).zfill(4)
+    ultimos_numeros = str(ultimos_numeros).zfill(4)
+
+    telefone = (f"({ddd}) 9{primeiros_numeros}-{ultimos_numeros}")
+    return telefone
